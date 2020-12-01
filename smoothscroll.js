86212a373f4a67ab2c4959b29cce1032292ff083
denvudd <68691654+denvudd@users.noreply.github.com>
2020-12-01 16:00:00 +0200

    Update smoothscroll.js

--- smoothscroll.js
+++ smoothscroll.js
@@ -5,11 +5,15 @@ $(function() {
         let elementId = $(this).data('scroll');
         let elementOffset = $(elementId).offset().top;
         
-        console.log(elementOffset);
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+        
+        $('#nav a').removeClass("active");
+        $this.addClass("active")
         
         $("html, body").animate({
             scrollTop: elementOffset -70
         }, 1000);
     });
 });
-
